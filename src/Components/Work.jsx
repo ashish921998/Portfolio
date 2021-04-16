7ee9813880a1be3291fa6fae359e6ab5f0cefb6b
Mayuresh Patwardhan <ashish.hudar@gmail.com>
2021-04-16 16:04:37 +0530

    added projects

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,7 +1,67 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Work.module.css";
+import { RiReactjsLine } from "react-icons/ri";
+import { SiMongodb } from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io";
+import { SiHtml5 } from "react-icons/si";
+import { SiCss3 } from "react-icons/si";
+import { FaNode } from "react-icons/fa";
 
 function Work() {
-  return <div></div>;
+  let iconStyles = { margin: "1% 2% 0 0", fontSize: "2.5em" };
+  return (
+    <div>
+      <Link className={styles.a} to="/">
+        Ashish
+      </Link>
+      <div className={styles.main}>
+        <div>
+          <h1>Dineout Clone</h1>
+          <h2>Restaurant table booking platform</h2>
+          <div>
+            <RiReactjsLine style={iconStyles} />
+            <SiMongodb style={iconStyles} />
+            <IoLogoJavascript style={iconStyles} />
+            <SiHtml5 style={iconStyles} />
+            <SiCss3 style={iconStyles} />
+            <FaNode style={iconStyles} />
+          </div>
+        </div>
+
+        <div className={styles.projects}>
+          <h1>Medium Clone</h1>
+          <h2>Blogging website</h2>
+          <div>
+            <RiReactjsLine style={iconStyles} />
+            <IoLogoJavascript style={iconStyles} />
+            <SiHtml5 style={iconStyles} />
+            <SiCss3 style={iconStyles} />
+          </div>
+        </div>
+
+        <div className={styles.projects}>
+          <h1>Myhours Clone</h1>
+          <h2>Time management webapp</h2>
+          <div>
+            <IoLogoJavascript style={iconStyles} />
+            <SiHtml5 style={iconStyles} />
+            <SiCss3 style={iconStyles} />
+          </div>
+        </div>
+
+        <div className={styles.projects}>
+          <h1>Happyfox Clone</h1>
+          <h2>Software-as-a-service(SaaS) support platform</h2>
+          <div>
+            <IoLogoJavascript style={iconStyles} />
+            <SiHtml5 style={iconStyles} />
+            <SiCss3 style={iconStyles} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Work;
