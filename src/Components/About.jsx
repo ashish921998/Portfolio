e7965e1b3f2ec90381543fab91b14894e5bbf5e6
Mayuresh Patwardhan <ashish.hudar@gmail.com>
2021-05-10 09:11:18 +0530

    added deployed links

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,21 +3,28 @@ import styles from "./About.module.css";
 import Pdf from "../Ashish Huddar.pdf";
 import { Player } from "@lottiefiles/react-lottie-player";
 import Navbar from "./Navbar";
+import profile from "../Images/ProfilePic.jpg";
 
 function About() {
   return (
     <div>
       <Navbar />
       <div style={{ marginTop: "3%" }}>
-        <Player
+        {/* <Player
           className={styles.lot}
           autoplay
           loop
           src="https://assets4.lottiefiles.com/packages/lf20_5k0romp3.json"
-        ></Player>
+        ></Player> */}
+        <img
+          src={profile}
+          alt="profile"
+          style={{ borderRadius: "50%", width: "25%", margin: "1% 0 1% 38%" }}
+        />
       </div>
       <div className={styles.main}>
         <h1>About</h1>
+
         <p>
           Hi, I am Ashish. I am a Passionate Full Stack Developer fueled by
           energy and enthusiasm, looking to break into the Software Development
--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -8,6 +8,7 @@ import { SiCss3 } from "react-icons/si";
 import { FaNode } from "react-icons/fa";
 import { FiExternalLink } from "react-icons/fi";
 import { AiFillGithub } from "react-icons/ai";
+import { SiNextDotJs } from "react-icons/si";
 import { Player } from "@lottiefiles/react-lottie-player";
 import Navbar from "./Navbar";
 import dineout from "../Images/1.png";
@@ -16,6 +17,7 @@ import medium from "../Images/medium.png";
 import myhours from "../Images/myhours.png";
 import sudoku from "../Images/sudoku.png";
 import ecom from "../Images/ecom.png";
+import sanj from "../Images/Sanj.png";
 
 function Work() {
   return (
@@ -32,6 +34,45 @@ function Work() {
         </div>
 
         <div className={styles.projects} style={{ marginTop: "10%" }}>
+          <div>
+            <h1>Sanjeevani</h1>
+            <p>Help seeking and help providing webapp</p>
+            <div>
+              <SiNextDotJs className={styles.iconStyles} />
+              <RiReactjsLine className={styles.iconStyles} />
+              <SiMongodb className={styles.iconStyles} />
+              <IoLogoJavascript className={styles.iconStyles} />
+              <SiHtml5 className={styles.iconStyles} />
+              <SiCss3 className={styles.iconStyles} />
+              <FaNode className={styles.iconStyles} />
+            </div>
+            <div style={{ display: "flex" }}>
+              <a
+                href="https://github.com/therealdrtroll/sanjeevani"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.link}
+              >
+                <AiFillGithub />
+                Code
+              </a>
+              <a
+                href="https://sanj-front.vercel.app/"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.link}
+              >
+                <FiExternalLink />
+                Demo
+              </a>
+            </div>
+          </div>
+
+          <div className={styles.img}>
+            <img src={sanj} alt="workImage" />
+          </div>
+        </div>
+        <div className={styles.projects}>
           <div>
             <h1>Sudoku Solver</h1>
             <p>Sudoku solving webapp</p>
@@ -64,6 +105,42 @@ function Work() {
           </div>
           <div className={styles.img}>
             <img src={sudoku} alt="workImage" />
+          </div>
+        </div>
+
+        <div className={styles.projects}>
+          <div>
+            <h1>Ecommerce </h1>
+            <p>Ecommerce app built with react and Mock API</p>
+            <div>
+              <RiReactjsLine className={styles.iconStyles} />
+              <IoLogoJavascript className={styles.iconStyles} />
+              <SiHtml5 className={styles.iconStyles} />
+              <SiCss3 className={styles.iconStyles} />
+            </div>
+            <div style={{ display: "flex" }}>
+              <a
+                href="https://github.com/ashish921998/React-Ecom-app"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.link}
+              >
+                <AiFillGithub />
+                Code
+              </a>
+              <a
+                href="https://ashishecom.netlify.app"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.link}
+              >
+                <FiExternalLink />
+                Demo
+              </a>
+            </div>
+          </div>
+          <div className={styles.img}>
+            <img src={ecom} alt="workImage" />
           </div>
         </div>
 
@@ -103,42 +180,6 @@ function Work() {
 
           <div className={styles.img}>
             <img src={dineout} alt="workImage" />
-          </div>
-        </div>
-
-        <div className={styles.projects}>
-          <div>
-            <h1>Ecommerce </h1>
-            <p>Ecommerce app built with react and Mock API</p>
-            <div>
-              <RiReactjsLine className={styles.iconStyles} />
-              <IoLogoJavascript className={styles.iconStyles} />
-              <SiHtml5 className={styles.iconStyles} />
-              <SiCss3 className={styles.iconStyles} />
-            </div>
-            <div style={{ display: "flex" }}>
-              <a
-                href="https://github.com/ashish921998/React-Ecom-app"
-                target="_blank"
-                rel="noreferrer"
-                className={styles.link}
-              >
-                <AiFillGithub />
-                Code
-              </a>
-              <a
-                href="https://ashishecom.netlify.app"
-                target="_blank"
-                rel="noreferrer"
-                className={styles.link}
-              >
-                <FiExternalLink />
-                Demo
-              </a>
-            </div>
-          </div>
-          <div className={styles.img}>
-            <img src={ecom} alt="workImage" />
           </div>
         </div>
 
